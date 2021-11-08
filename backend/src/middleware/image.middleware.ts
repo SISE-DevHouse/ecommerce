36738b9638dca0cc31ebd10f6b3389157cb71874
Cristian <cristian.puma.es6@gmail.com>
2021-11-08 17:24:59 -0500

    Miffleware image.

---
+++ backend/src/middleware/image.middleware.ts
@@ -0,0 +1,22 @@
+import { extname } from "path";
+
+// Filtra imagen, solo devuelve true si es una imagen permitida caso contrario false
+export const ImageFileFilter = (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return callback(new Error('Only image files are allowed!'), false);
+    }
+    callback(null, true);
+};
+
+
+// The editFileName function has the same structure but creates a custom filename 
+// using the original name, the file extension and four random numbers.
+export const EditFileName = (req, file, callback) => {
+    const name = file.originalname.split('.')[0];
+    const fileExtName = extname(file.originalname);
+    const randomName = Array(4)
+        .fill(null)
+        .map(() => Math.round(Math.random() * 16).toString(16))
+        .join('');
+    callback(null, `${name}-${randomName}${fileExtName}`);
+};
