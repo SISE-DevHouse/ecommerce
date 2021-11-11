f034a9b86ba6cb3a4ca628027910c490229ee538
Cristian <cristian.puma.es6@gmail.com>
2021-11-11 00:03:54 -0500

    ok

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -26,15 +26,21 @@ const defaultOptions = {
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type:"postgres",
-      url: "postgres://czxzflbeobavph:cb4a7e2297451dbbbb13bea2453053917893fc8bf305c19b9926a061e1e86eb9@ec2-44-199-85-33.compute-1.amazonaws.com:5432/d2gcije7c5cj4m",
-      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
-      synchronize: true, 
-      port: 5432,
-      ssl: false,
-    }),
+    //TypeOrmModule.forRoot({
+    //  type:"postgres",
+    //  url: "postgres://czxzflbeobavph:cb4a7e2297451dbbbb13bea2453053917893fc8bf305c19b9926a061e1e86eb9@ec2-44-199-85-33.compute-1.amazonaws.com:5432/d2gcije7c5cj4m",
+    //  entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+    //  synchronize: true, 
+    //  port: 5432,
+    //  ssl: false,
+    //}),
 
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(SQLITE_PATH, 'dbSise.sqlite3'),
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+    }),
     UsersModule,
     AuthModule,
   ],
