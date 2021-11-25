b2e10271eb3712ed621511ed334dd598c8eda788
LocoM17 <sisebas18@gmail.com>
2021-11-24 21:39:40 -0500

    detalleventaV2

--- backend/src/components/detalleventas/detalleventas.controller.ts
+++ backend/src/components/detalleventas/detalleventas.controller.ts
@@ -81,4 +81,232 @@ export class DetalleventasController {
             }
         );
     }
+
+    @Get()
+    async Gets(@Headers() headers, @Query() Detalleventa: DetalleventaEntity): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+                // Validamos que los datos sean los necesarios.
+                if (headerToken && (headerToken.role == 'ADMIN' || headerToken.role == 'SUPPORT' || headerToken.role == 'OWNER') && Detalleventa) {
+
+                    // Ejecutamos el servicio de obtener sailingAnalities.
+                    return this._detalleService.Gets(Detalleventa);
+
+                } else {
+                    if (Number(Detalleventa.id) === Number(headerToken.id)) {
+                        // Ejecutamos el servicio de obtener sailingAnalities.
+                        return this._detalleService.Gets(Detalleventa);
+                    } else {
+                        throw new Error('MISSING_FIELS');
+                    }
+                }
+
+            }
+        ).then(
+            (results: DetalleventaEntity[]) => {
+
+                // Retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: results
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // Caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+    }
+    
+    @Get(':id')
+    async Get(@Headers() headers, @Param('id') id): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (result: Boolean) => {
+
+                // Convertimos a numeros.
+                let detalleId = Number(id);
+
+                // Validamos que los datos recibidos sean los correctos.
+                if (detalleId && headerToken.id) {
+
+                    // Validamos si es un admin o un support.
+                    if (headerToken.role == 'ADMIN' || headerToken.role == 'SUPPORT') {
+                        // No hacemos nada.
+                    } else {
+                        // caso contrario verificamos si el id es el mismo.
+                        if (headerToken.id !== id) throw new Error('ERROR_USERID_FAIL');
+                    }
+
+                    return this._detalleService.Get(detalleId);
+
+
+                } else {
+                    // caso contrario retornamos un error
+                    throw new Error('MISSING_FIELS');
+                }
+
+            }
+        ).then(
+            (resultGet: DetalleventaEntity) => {
+
+                // retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: resultGet
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+    }
+
+    @Put(':id/update')
+    async UpdateDetalle(@Headers() headers, @Param('id') id, @Body() detalle: DetalleventaEntity): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+
+                // que solo puedan registrar un nuevo usuario los administradores o support.
+                if (!(headerToken.role == 'ADMIN' || headerToken.role == 'SUPPORT')) {
+                    throw new Error('Se esta intentado registrar con un rol no valido.');
+                }
+
+                if (!isNaN(id) && detalle && detalle.cantidad && detalle.precio) {
+
+                    detalle.id = Number(id);
+
+                    delete detalle.userIdCreated;
+                    delete detalle.dateCreated;
+                    detalle.userIdUpdated = headerToken.id;
+                    detalle.dateUpdated = GetDate();
+
+                    detalle.status = Boolean(detalle.status);
+
+
+
+                    // retornamos la respuesta del servicio.
+                    return this._detalleService.UpdateDetalleId(detalle);
+                } else {
+                    // caso contrario retornamos un error
+                    throw 'MISSING_FIELS';
+                }
+            }
+        ).then(
+            (resultUpdate: DetalleventaEntity) => {
+
+                // retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: resultUpdate
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+
+    }
+
+    @Delete(':id/delete')
+    async delete(@Headers() headers, @Param('id') id): Promise<any> {
+
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+
+                // que solo puedan registrar un nuevo usuario los administradores o support.
+                if (!(headerToken.role == 'ADMIN' || headerToken.role == 'SUPPORT')) {
+                    throw new Error('Se esta intentado registrar con un rol no valido.');
+                }
+
+                // Validamos que esten los campos necesarios.
+                if (Number(id)) {
+                    // retornamos la respuesta del servicio.
+                    return this._detalleService.Delete(id);
+                } else {
+                    // aso contrario retornamos un error
+                    throw new Error('MISSING_FIELS');
+                }
+            }
+        ).then(
+            (resultUpdate: DetalleventaEntity) => {
+
+                // retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: resultUpdate
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+    }
 }
--- backend/src/components/detalleventas/detalleventas.service.ts
+++ backend/src/components/detalleventas/detalleventas.service.ts
@@ -35,4 +35,114 @@ export class DetalleventasService {
             }
         );
     }
+
+    async Gets(detalle: DetalleventaEntity): Promise<DetalleventaEntity[]> {
+
+        // Hacemos where por todos los campos de la entidad
+        return await this.DetalleventaRepository.find({
+            where: [
+                // name && surname && nick && email
+                {
+                    id: (detalle.id || Like('%' + '%')),
+                    id_venta: (detalle.id_venta || Like('%' + '%')),
+                    id_producto: (detalle.id_producto || Like('%' + '%')),
+                    //cantidad: (detalle.ca || Like('%' + '%')),
+                    //nick: Like('%' + (user.nick || '') + '%'),
+                    //name: Like('%' + (user.name || '') + '%'),
+                    //lastName: Like('%' + (user.lastName || '') + '%'),
+                    //phoneNumber: Like('%' + (user.phoneNumber || '') + '%'),
+                    //role: Like('%' + (user.role || '') + '%'),
+                    status: Not(false)
+                }
+            ]
+        }).then(
+            (result: DetalleventaEntity[]) => {
+                return result;
+            }
+        )
+    }
+
+    async Get(id: number): Promise<DetalleventaEntity> {
+
+        // Hacemos una busqueda por id
+        return await this.DetalleventaRepository.findOne({
+            where: {
+                id: id,
+                status: Not(false)
+            }
+        }).then(
+            resultFind => {
+                // Validamos si encontro al usuario.
+                if (!resultFind) throw new Error('user_does_not_exist');
+                // Vaciamos el campo del password.
+                //resultFind.password = null;
+                // retornamos el objeto.
+                return resultFind;
+            });
+
+    }
+
+        async UpdateDetalleId(detalle: DetalleventaEntity): Promise<DetalleventaEntity> {
+        // Contrasela antigua
+        let contraseniaOld = '';
+
+        // Hacemos una busqueda por id
+        return await this.DetalleventaRepository.findOne({
+            where: [
+                // hacemos un where donde buscamos por id.
+                { id: detalle.id }
+            ]
+        }).then(resultFind => {
+
+            // Validamos si encontro al usuario.
+            if (!resultFind) throw new Error('user_does_not_exist');
+
+            return this.DetalleventaRepository.update(detalle.id, detalle);
+            }
+        ).then(resultUpdate => {
+
+            if (!resultUpdate) throw new Error('userRepository.update no respondio como esperabamos.');
+
+            // borramos el password por seguridad.
+            // delete user.password;
+            //user.password = '';
+
+            // Envio respuesta con el resultado recibido del ultimo paso
+            return detalle;
+        });
+    }
+
+    async Delete(detalleId: number): Promise<DetalleventaEntity> {
+
+        let detalle: DetalleventaEntity = new DetalleventaEntity();
+
+        // Eliminamos de la base de dato al usuario.
+        return await this.DetalleventaRepository.findOne({
+            where: [
+                // hacemos un where donde buscamos por id.
+                { id: detalleId }
+            ]
+        }).then(resultFind => {
+            // Validamos si encontro al usuario.
+            if (!resultFind) throw new Error('user_does_not_exist');
+
+            // Seteamos al usuario.
+            detalle = resultFind;
+            // Desactivamos el estado.
+            detalle.status = false;
+
+            // Actualizamos al usuario
+            return this.DetalleventaRepository.update(detalle.id, detalle);
+        }).then(
+            resultSave => {
+
+                // Validamos si se actualizo correctamente.
+                if (!resultSave) throw new Error('error_user_save');
+                // Borramos el password.
+                //user.password = '';
+
+                return detalle;
+            }
+        )
+    }
 }
