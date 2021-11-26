c1d85793ac6705b831863434a2c5ea3b8879b71c
Cristian <cristian.puma.es6@gmail.com>
2021-11-25 20:19:30 -0500

    se configuro el modulo

--- backend/src/components/entrada-producto/entrada-producto.module.ts
+++ backend/src/components/entrada-producto/entrada-producto.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EntradaProductoService } from './entrada-producto.service';
 import { EntradaProductoController } from './entrada-producto.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntradaProductoEntity } from 'src/models/entrada-producto.entity';
 
 @Module({
   providers: [EntradaProductoService],
-  controllers: [EntradaProductoController]
+  controllers: [EntradaProductoController],
+  //Importamos el TypeOrm con el modulo a usar, para que funcione en el servicio.
+  imports: [
+    TypeOrmModule.forFeature([EntradaProductoEntity]),
+  ],
+  // Exportamos el servicio para que lo usemos desde otro lado.
+  exports: [EntradaProductoService],
 })
 export class EntradaProductoModule {}
