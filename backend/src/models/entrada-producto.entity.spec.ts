0618d44e076701e79e45d8dd706344e9ea0c03ce
Cristian <cristian.puma.es6@gmail.com>
2021-11-25 20:16:03 -0500

    se agregaron las entidades

--- backend/src/models/entrada-producto.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EntradaProductoEntity } from './entrada-producto.entity';
-
-describe('EntradaProductoEntity', () => {
-  it('should be defined', () => {
-    expect(new EntradaProductoEntity()).toBeDefined();
-  });
-});
--- backend/src/models/entrada-producto.entity.ts
+++ backend/src/models/entrada-producto.entity.ts
@@ -1,1 +1,28 @@
-export class EntradaProductoEntity {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToOne, Double } from 'typeorm';
+
+@Entity('EntradaProducto')
+export class EntradaProductoEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column(
+        // Puede ser null falso
+        { nullable: false })
+    idProducto: number; 
+
+    @Column(
+        // Puede ser null falso
+        { nullable: false })
+    cantidad: number; 
+    
+    @Column(
+        // Puede ser null falso
+        { nullable: false })
+    costo_unitario: number; 
+    
+    @Column(
+        // Puede ser null falso
+        { nullable: false })
+    fecha: string; 
+}
