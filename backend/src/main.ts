4d1366ad6f1d21fdfb304e736f561108932edc5b
Cristian <cristian.puma.es6@gmail.com>
2021-11-29 20:38:29 -0500

    ok

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
+  const options = {
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false,
+    'optionsSuccessStatus': 204,
+    'credentials': true,
+  };
+  
+  // Habilitamos el CORS
+  // app.use(cors(options))
+  app.enableCors(options);
+
+  
   await app.listen(3000);
 }
 bootstrap();
